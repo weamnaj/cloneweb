361a865a55b4ac26c36d3e22acf1a474a201db05
AhmedShantti <20220848@student.alazhar.edu.ps>
2024-09-24 19:49:14 +0300

    adding h1 = firstFile

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -28,8 +28,12 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      <div>
+        <h1>Hello from Ahmed Device</h1>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
