b47436886c94f2f9c5dfcb53e6e89af3e5c861fe
Mansour Al-Dadah <45122396+mansour-aldadah@users.noreply.github.com>
2024-09-28 20:22:27 +0300

    Mansour (#6)
    * Start From Here
    
    * Add footer with its style and some of pages in footer

---
+++ src/pages/ContactUs/ContactUs.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function ContactUs() {
+  return (
+    <section className="contact-us">
+      <h3>اتصل بنا</h3>
+      <hr />
+    </section>
+  );
+}
