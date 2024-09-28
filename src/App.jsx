10651071098ab8fef222952ff48e6571bd71348b
AhmedShantti <20220848@student.alazhar.edu.ps>
2024-09-28 20:09:21 +0300

    Home Page Sturcture and Style

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Ahmed from "./assets/firstFile-Ahmed";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -33,8 +32,6 @@ function App() {
       <div>
         <h1>Hello from Ahmed Device</h1>
       </div>
-
-      <Ahmed />
     </>
   );
 }
