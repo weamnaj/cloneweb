1a9f8c51fd2854a7393c3637510067ceb84af8ba
Ahmed Al-Shantti <118689736+AhmedShantti@users.noreply.github.com>
2024-09-29 13:01:53 +0300

    Home Page Sturcture and Style (#9)

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,43 @@
 import "./App.css";
+<<<<<<< HEAD
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+
+      <div>
+        <h1>Hello from Ahmed Device</h1>
+      </div>
+    </>
+  );
+=======
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return <></>;
+>>>>>>> c2e201e0581fa37c26342b97f948e5b7c958900b
 }
 
 export default App;
