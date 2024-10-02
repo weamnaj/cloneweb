2bb86c03372db3144e7999cd5709941a2e6e1741
Mansour  Al-Dadah <mansouraldadah@gmail.com>
2024-10-02 17:08:46 +0300

    Some Updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import Footer from "./components/shared/footer/footer";
+import NotFound from "./pages/NotFound/NotFound";
+import HomePage from "./pages/Home/homePage";
 export default function App() {
-  return <></>;
+  return (
+    <>
+      <NotFound />
+      <Footer />
+    </>
+  );
 }
