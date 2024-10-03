c34b0a5b26e88ee7a9b2ae1059c9db664592f0be
AhmedShantti <20220848@student.alazhar.edu.ps>
2024-10-03 17:06:17 +0300

    Sections List Sturcture and Style (without functionality)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
-
-import "bootstrap/dist/css/bootstrap.min.css";
+import HomePage from "./pages/home/homePage.jsx";
+/* import "bootstrap/dist/css/bootstrap.min.css"; */
 
 export default function App() {
-  return;
+  return <HomePage />;
 }
