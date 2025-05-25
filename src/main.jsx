7d40b40cb0170311b213cc7b3dedbe6576d4fe73
weam nimer abdulkareem jarbou <weamjarbou@gmail.com>
2025-05-25 17:51:35 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { Honeybadger, HoneybadgerErrorBoundary } from "@honeybadger-io/react"
 
+const config = {
+  apiKey: "hbp_5mIizkcYVGWdi4Kepd28JZQLoPqCLl1WuVHP",
+  environment: "production"
+}
+
+const honeybadger = Honeybadger.configure(config)
+
+ReactDOM.render(<HoneybadgerErrorBoundary honeybadger={honeybadger}><App /></HoneybadgerErrorBoundary>, document.getElementById("root"))
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
